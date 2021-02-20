65d6a6f40adf0910f9c7befaeef6d669d9062479
arjanmitra <arjanmitra@hotmail.com>
2021-02-20 11:57:34 -0500

    arjan changes

--- server.js
+++ server.js
@@ -1,76 +1,80 @@
 const {
-    syncAndSeed, 
-    models: {Restaurant, User, Reservation}
-} = require('./db/db')
+  syncAndSeed,
+  models: { Restaurant, User, Reservation },
+} = require('./db/db');
 
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const path = require('path');
 
-
 app.use(require('body-parser').json());
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
-app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res, next) =>
+  res.sendFile(path.join(__dirname, 'index.html'))
+);
 
-app.get('/api/users', async(req, res, next)=> {
+app.get('/api/users', async (req, res, next) => {
   try {
     res.send(await User.findAll());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/restaurants', async(req, res, next)=> {
+app.get('/api/restaurants', async (req, res, next) => {
   try {
     res.send(await Restaurant.findAll());
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.get('/api/users/:userId/reservations', async(req, res, next)=> {
+app.get('/api/users/:userId/reservations', async (req, res, next) => {
   try {
-    res.send(await Reservation.findAll({ where: { userId: req.params.userId }}));
-  }
-  catch(ex){
+    res.send(
+      await Reservation.findAll({ where: { userId: req.params.userId } })
+    );
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.post('/api/users/:userId/reservations', async(req, res, next)=> {
+app.post('/api/users/:userId/reservations', async (req, res, next) => {
   try {
-    res.status(201).send(await Reservation.create({ userId: req.params.userId, restaurantId: req.body.restaurantId}));
-  }
-  catch(ex){
+    res
+      .status(201)
+      .send(
+        await Reservation.create({
+          userId: req.params.userId,
+          restaurantId: req.body.restaurantId,
+        })
+      );
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.delete('/api/reservations/:id', async(req, res, next)=> {
+app.delete('/api/reservations/:id', async (req, res, next) => {
   try {
     const reservation = await Reservation.findByPk(req.params.id);
     await reservation.destroy();
     res.sendStatus(204);
-  }
-  catch(ex){
+  } catch (ex) {
     next(ex);
   }
 });
 
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
   console.log(chalk.red(err.stack));
   res.status(500).send({ error: err.message });
 });
-const port = process.env.PORT || 3000;
-
-const init = async()=> {
+const port = process.env.PORT || 3001;
+const init = async () => {
   await syncAndSeed();
-  app.listen(port, ()=> console.log(`listening on port ${port}`));
-}
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
 
 init();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const init = async() => {
-    try {
-        const users = (await axios.get('/api/users')).data
-        const restaurants = (await axios.get('/api/restaurants')).data
-        console.log(users, restaurants)
-    }
-    catch(ex){
-        console.log(ex)
-    }
-}
-init()
+const usersDiv = document.querySelector('#users');
+
+const renderUsers = async (users) => {
+  try {
+    const usersDivUlChild = document.createElement('ul');
+    users.map((user) => {
+      const li = document.createElement('li');
+      li.innerHTML = user.name;
+      usersDivUlChild.appendChild(li);
+    });
+    usersDiv.appendChild(usersDivUlChild);
+    //usersDiv.innerHTML = users.map
+
+    //JSON.stringify(users);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = async () => {
+  try {
+    const users = (await axios.get('/api/users')).data;
+    const restaurants = (await axios.get('/api/restaurants')).data;
+    renderUsers(users);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+init();
